02381bafbdba49a870c51985a8d7ab071daedc4d
developer-tamim <tawsiftamim24@gmail.com>
2024-03-09 15:35:49 +0600

    form validation

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import VeeValidatePlugin from './includes/validation';
 
-import './assets/base.css'
-import './assets/main.css'
+import './assets/base.css';
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+
+app.use(createPinia());
+app.use(router);
+app.use(VeeValidatePlugin);
+
+app.mount('#app');
